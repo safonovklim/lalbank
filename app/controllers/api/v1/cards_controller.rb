1d227329f91afe7db1bbd00926335413ad615f97
safonovklim <me@safonovklim.ru>
2016-12-11 04:03:44 +0300

    (security fix) Added [GET] /users/:id/cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class CardsController < ApiController
+    before_action :authenticate_employee, only: [:index]
     before_action only: [:create] {
       authenticate_employee("security_staff")
     }
